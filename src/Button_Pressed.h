76272bb5fc3e9d49353505151d130ab203ae9af8
HomeMadeRobots <homemade.bots@gmail.com>
2020-04-22 10:51:57 +0200

    Transfert to Embedded_C_Framework

---
+++ src/Button_Pressed.h
@@ -0,0 +1,8 @@
+#ifndef BUTTON_PRESSED_H
+#define BUTTON_PRESSED_H
+
+/* Event_Interface */
+/* Sent when a push button is pressed. */
+typedef void (*Button_Pressed) ( void );
+
+#endif
---
+++ src/Button_Released.h
@@ -0,0 +1,8 @@
+#ifndef BUTTON_RELEASED_H
+#define BUTTON_RELEASED_H
+
+/* Event_Interface */
+/* Sent when a push button is released. */
+typedef void (*Button_Released) ( void );
+
+#endif
